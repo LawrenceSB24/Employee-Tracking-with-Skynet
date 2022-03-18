4b4b249ebcc2946a8ff6f845e4d8b218c223760f
LawrenceSB24 <lawrs2022@gmail.com>
2022-03-18 18:46:59 -0400

    Returned PORT variable for MySQL connection

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const inquirer = require('inquirer');
 // console.table package imported
 require('console.table');
 
-// // Local port for Heroku uploading
-// const PORT = process.env.PORT || 3001;
+// Local port for Heroku uploading
+const PORT = process.env.PORT || 3001;
 
 // Initializing express
 const app = express();
@@ -26,7 +26,8 @@ const db = mysql.createConnection(
         host: "localhost",
         user: "root",
         password: "password",
-        database: "employee_db"
+        database: "employee_db",
+        port: PORT
     },
     console.log("Connection is a go!")
 );
